1b68383cdcfca20d8a0a8b9fd1202ae7271302ac
JeremiahIT <jeremica03132001@gmail.com>
2025-08-11 20:06:28 +0800

    Update vite.config.js

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -5,3 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+  resolve: {
+    alias: [
+      { find: '/mainpage', replacement: path.resolve(__dirname, 'src/mainpage') },
+      { find: '/permits', replacement: path.resolve(__dirname, 'src/permits') },
+      { find: '/config', replacement: path.resolve(__dirname, 'src/config') },
+      { find: '/assets', replacement: path.resolve(__dirname, 'src/assets') },
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+    ],
+  },
+})
