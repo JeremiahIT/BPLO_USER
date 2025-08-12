8dd0215cbe806d5121e4da7453372e7e1418dd80
JeremiahIT <jeremica03132001@gmail.com>
2025-08-13 07:40:30 +0800

    awdawdadwaa

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,17 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
   resolve: {
-    alias: [
-      { find: '/mainpage', replacement: path.resolve(__dirname, 'src/mainpage') },
-      { find: '/permits', replacement: path.resolve(__dirname, 'src/permits') },
-      { find: '/config', replacement: path.resolve(__dirname, 'src/config') },
-      { find: '/assets', replacement: path.resolve(__dirname, 'src/assets') },
-      { find: '@', replacement: path.resolve(__dirname, 'src') },
-    ],
-  },
+    alias: {
+      '/mainpage': path.resolve(__dirname, 'src/mainpage'),
+      '/permits': path.resolve(__dirname, 'src/permits'),
+      '/config': path.resolve(__dirname, 'src/config'),
+      '/assets': path.resolve(__dirname, 'src/assets'),
+      '@': path.resolve(__dirname, 'src'),
+    }
+  }
 })
