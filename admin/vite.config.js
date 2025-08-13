8c56edf8f3bf9c108705e2f37a77e8c5083b87a4
JeremiahIT <jeremica03132001@gmail.com>
2025-08-14 04:58:00 +0800

    for admin render

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -6,11 +6,12 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '/mainpage': path.resolve(__dirname, 'src/mainpage'),
-      '/permits': path.resolve(__dirname, 'src/permits'),
-      '/config': path.resolve(__dirname, 'src/config'),
-      '/assets': path.resolve(__dirname, 'src/assets'),
-      '@': path.resolve(__dirname, 'src'),
-    }
+  mainpage: path.resolve(__dirname, 'src/mainpage'),
+  permits: path.resolve(__dirname, 'src/permits'),
+  config: path.resolve(__dirname, 'src/config'),
+  assets: path.resolve(__dirname, 'src/assets'),
+  '@': path.resolve(__dirname, 'src'),
+}
+
   }
 })
