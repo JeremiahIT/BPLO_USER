4c29ae286ebe3a321af45e04df587ec6dbc8b440
JeremiahIT <jeremica03132001@gmail.com>
2025-08-14 05:14:17 +0800

    changes in app

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';  
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Dashboard from "/mainpage/dashboard";
-import NewPermit from "/permits/new_permit";
-import RenewalPermit from "/permits/renewal_permit";
+import Dashboard from "./mainpage/dashboard";
+import NewPermit from "./permits/new_permit";
+import RenewalPermit from "./permits/renewal_permit";
 
 function App() {
   return (
